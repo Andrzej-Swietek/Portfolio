636f85a8487512ecb6660a6534937e1c15dd9fad
Andrzej Świętek <mr.andi321@gmail.com>
2019-11-26 11:53:43 +0100

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+export default{
+    init: function(){
+        if('serviceWorker' in navigator){
+            navigator.serviceWorker
+                .register('/service-worker.js')
+                .then(function(){
+                    console.log('Service Worker Registered')
+                })
+                .catch(function(e){
+                    console.log("Error while registering service worker", e)
+                })
+        }else{
+            alert("Sorry your browser does not support PWA - demo")
+        }   
+    }
+}
